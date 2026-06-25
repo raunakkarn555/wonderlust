020b65857164293e57c387ffc2ab43b9526eb589
Raunak Karn <raunakkarn555@gmail.com>
2026-06-25 15:24:25 +0530

    fix: comma error in secret message

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, "public")));
 
-const secret = process.env.SECRET,
+const secret = process.env.SECRET;
 
 
 const store = MongoStore.create({
